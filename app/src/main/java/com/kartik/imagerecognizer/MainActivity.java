cd3148e882b7c240279c2a90bb762edc51e99129
kartik.gujarati <kartik.gujarati@malauzai.com>
2017-10-08 23:10:33 -0500

    Initial Commit

---
+++ app/src/main/java/com/kartik/imagerecognizer/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kartik.imagerecognizer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+/* -------------------------------------- Lifecycle Methods -------------------------------------- */
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+/* ------------------------------------ End Lifecycle Methods ------------------------------------ */
+}
